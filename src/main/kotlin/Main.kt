3ab8e073aacf512dd29028dcc98d9e935e8845f6
Ramesh Shyaman <rameshshyaman@gmail.com>
2022-12-29 13:36:29 +0530

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,41 +25,69 @@ data class ServiceImpl(val source : String) : Service{
      }
 }
 
-class MethodInterceptor<T>(private val target: T) : InvocationHandler {
 
-    override fun invoke(proxy: Any, method: Method, args: Array<out Any>): Any {
-        println("Method Name : ${method.name} ")
-        print("Arguments:")
-        args.forEach {
-            print(it.toString())
-            print(',')
+class ServiceFactory< out T0>(private val targetClass : Class<T0>){
+
+    private val _params : MutableList<Any> = mutableListOf()
+    private lateinit var _interfaces : Array<Class<*>>
+
+    private class MethodInterceptor<T>(private val target: T) : InvocationHandler {
+
+        override fun invoke(proxy: Any, method: Method, args: Array<out Any>): Any {
+            println("Method Name : ${method.name} ")
+            print("Arguments:")
+            args.forEach {
+                print(it.toString())
+                print(',')
+            }
+            println()
+
+
+            val res = method.invoke(target, *args)
+            println(res.toString())
+            return res
         }
-        println()
 
+    }
 
-        val res = method.invoke(target, *args)
-        println(res.toString())
-        return res
+    private fun createServiceProxy(target: T0, interfaces: Array<Class<*>>): T0 {
+        return Proxy.newProxyInstance(
+            target!!::class.java.classLoader,
+            interfaces,
+            MethodInterceptor(target)
+        ) as T0
+
+    }
+
+    fun addParam(param : String): ServiceFactory<T0> {
+        this._params.add(param)
+        return this
+    }
+
+    fun setInterfaces(interfaces : Array<Class<*>>): ServiceFactory<T0> {
+        _interfaces = interfaces
+        return this
+
+    }
+
+    fun build(): T0 {
+        return createServiceProxy(
+            targetClass.getConstructor(
+                *_params.map { it::class.java }.toTypedArray()
+            ).newInstance(*_params.toTypedArray()) ,
+            _interfaces)
     }
 
 }
 
-inline fun <reified T> createServiceProxy(target : T, itr : Class<T> ) : T{
-    val proxy = Proxy.newProxyInstance(
-        itr.classLoader,
-        arrayOf(itr,),
-        MethodInterceptor(target)
-    )
-    return proxy as T
-}
 
 
 
 fun main(args: Array<String>) {
-    val service : Service = createServiceProxy(
-        ServiceImpl("localhost") ,
-        Service::class.java
-    )
+    val service : Service = ServiceFactory(ServiceImpl::class.java)
+        .addParam("localhost")
+        .setInterfaces(arrayOf(Service::class.java))
+        .build()
 
     service.add(1.0,2.0)
     service.sub(10.0,2.0)
